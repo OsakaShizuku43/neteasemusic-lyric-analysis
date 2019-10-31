84405b2608eb8ace1dd05a5c138c103a438ef56f
Zimin Cao <caozmbill@gmail.com>
2019-10-30 23:33:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# NeteaseMusic Lyric Analysis
+## Introduction
+Analyze the composition of words in your NeteaseMusic favourite song list (Japanese songs only for this project).
+
+The following aspects are customizable:
+- Part of speech to include in the word cloud (noun, adjective, verbs, ...)
+- Specific words to exclude
+- Specific characters to exclude
+
+## Acknowledgement
+- NeteaseMusic API: [Binaryify/NeteaseCloudMusicApi](https://github.com/Binaryify/NeteaseCloudMusicApi)
+- MeCab (Extracting words from lyric): https://taku910.github.io/mecab/#usage-tools
+- word_cloud: https://github.com/amueller/word_cloud
+
+Without the above projects, this project won't be able to come true. Thank you!
+
+## Requirements
+- mecab-python3
+- word_cloud
+- matplotlib
+- requests
+
+## Get it working
+1. Clone the API, [Binaryify/NeteaseCloudMusicApi](https://github.com/Binaryify/NeteaseCloudMusicApi), and run it following its documentation.
+2. Install required packages for this project (listed in above section).
+3. In `main.py`, put credential of your NeteaseMusic account, and specify settings.
+4. `python main.py` and enjoy!
---
+++ main.py
@@ -0,0 +1,107 @@
+import re
+import requests
+
+from wordcloud import WordCloud
+import MeCab
+import matplotlib.pyplot as plt
+
+API_URL = 'http://localhost:3000'
+NETEASE_EMAIL = ''
+NETEASE_PASSWORD = ''
+FILTER_CHARS = '　、。！？<＞()（）…・\''
+FILTER_WORDS = '''
+ない
+てる
+する
+れる
+る
+いる
+たい
+なる
+ちゃう
+られる
+ある
+よう
+こと
+どこ
+ここ
+そう
+くる
+いい
+それ
+'''.strip().split('\n')
+FILTER_TYPES = ['名詞', '動詞', '形容詞', '副詞']
+
+## Login to NeteaseMusic
+s = requests.session()
+user_info = s.get(API_URL + '/login?email={}&password={}'.format(NETEASE_EMAIL, NETEASE_PASSWORD)).json()
+uid = user_info.get('account').get('id')
+print('User id:', uid)
+
+## Get favourite song list
+playlist = s.get(API_URL + '/likelist?uid={}'.format(uid)).json()
+fav_song_ids = playlist.get('ids')
+print('Fav song list retrieved')
+if fav_song_ids is None:
+    print(playlist)
+    exit()
+
+## Get lyrics for each song
+lyrics = []
+for song_id in fav_song_ids:
+    lyric_info = s.get(API_URL + '/lyric?id={}'.format(song_id)).json()
+    if 'lrc' not in lyric_info:
+        continue
+    lyric = lyric_info.get('lrc').get('lyric')
+    if len(lyric) > 0:
+        lyrics.append(lyric)
+        print('Lyric for song {} downloaded...'.format(song_id))
+print('Lyrics retrieved')
+
+## Process lyrics into correct format for word analysis
+##  Get rid of pronunciation hint (Japanese) and lrc file timestamp
+def process_lrc(line):
+    if '作曲' in line or '作詞' in line:
+        return ''
+    re_lrc = re.compile(r'\[.+\]')
+    re_bracket = re.compile(r'\(.+\)')
+    re_jp_bracket = re.compile(r'（([^）.]+)）')
+    line = re.sub(re_lrc, '', line)
+    line = re.sub(re_bracket, '', line)
+    line = re.sub(re_jp_bracket, '', line)
+    return line
+for i in range(len(lyrics)):
+    lyrics[i] = '\n'.join(map(process_lrc, lyrics[i].split('\n')))
+print('Lyrics processed')
+
+## Extract for individual words
+##  Uses MeCab to retrive dictionary form for each word and take nouns, adjective, verbs only
+mc = MeCab.Tagger("-Ochasen")
+re_english = re.compile(r'[A-Za-z]')
+extracted_words = []
+counter = 0
+for lyric in lyrics:
+    counter += 1
+    print('Extracting words in lyric: {}...'.format(counter))
+    for word_info_line in mc.parse(lyric.strip()).split('\n'):
+        word_info = word_info_line.split('\t')
+        if len(word_info) < 4:
+            continue
+        word_orig = word_info[2]
+        word_type = word_info[3]
+        if word_orig not in FILTER_CHARS and \
+            re.match(re_english, word_orig) is None and \
+                word_orig not in FILTER_WORDS and \
+                    len(list(filter(lambda t: t in word_type, FILTER_TYPES))) > 0:
+            extracted_words.append(word_orig)
+print('Lyrics analysis complete')
+
+## Create a word cloud
+print('Generating word cloud...')
+font = './Kyokasho.ttc'
+wc = WordCloud(collocations=True, font_path=font, width=2850, height=1800, margin=4).generate('\n'.join(extracted_words))
+print('Done')
+
+plt.imshow(wc)
+plt.axis("off")
+plt.show()
